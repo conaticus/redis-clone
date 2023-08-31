c47630209ecdc82803fe7a448dcba36f4537f76a
Sam <enquiry.conaticus@gmail.com>
2023-08-31 12:03:34 +0100

    pong

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
-use std::net::TcpListener;
+use std::error::Error;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+fn accept_connection(mut conn: TcpStream) -> Result<(), Box<dyn Error>> {
+    conn.write("pong!".as_bytes())?;
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let address = "127.0.0.1:6379";
+    let listener = TcpListener::bind(address)?;
+    println!("Listening at {}", address);
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
+            Ok(stream) => accept_connection(stream)?,
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+
+    Ok(())
 }
