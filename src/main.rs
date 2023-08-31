e959aaee4549afa3c3fb425e576065bca9cc7c0c
Sam <enquiry.conaticus@gmail.com>
2023-08-31 12:53:38 +0100

    multiple pings

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,31 @@ use std::error::Error;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 
-fn accept_connection(mut conn: TcpStream) -> Result<(), Box<dyn Error>> {
-    conn.write("+PONG\r\n".as_bytes())?;
+async fn accept_connection(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
+    loop {
+        let mut buffer = [0; 512];
+        match stream.read(&mut buffer) {
+            Ok(0) => break,
+            Ok(_) => stream.write(b"+PONG\r\n")?,
+            Err(e) => {
+                println!("Error occured: {}", e);
+                break;
+            }
+        };
+    }
 
     Ok(())
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     let address = "127.0.0.1:6379";
     let listener = TcpListener::bind(address)?;
     println!("Listening at {}", address);
     
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) => accept_connection(stream)?,
+            Ok(stream) => accept_connection(stream).await?,
             Err(e) => {
                 println!("error: {}", e);
             }
